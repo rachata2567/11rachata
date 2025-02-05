05867d553b037750ce1f77c15481e03b6e204562
rachata2567 <tuynuy.kankasem@gmail.com>
2025-02-05 14:12:10 +0700

    first commit

--- src/app/dataapi.service.ts
+++ src/app/dataapi.service.ts
@@ -11,24 +11,24 @@ export class DataapiService {
 
   //ฟังก์ชั่นเชื่อมต่อ api ระบบบันทึกข้อมูล
   addproduct(formData: any) {
-    return this.http.post('http://localhost/crudimg/upload.php', formData);
+    return this.http.post('http://localhost/crudapi/upload.php', formData);
   }  
 
   //เชื่อมต่อ api แสดงข้อมูล
   showproduct() {
-    return this.http.get('http://localhost/crudimg/read.php');
+    return this.http.get('http://localhost/crudapi/read.php');
   }
 
   //เชื่อมต่อ api ลบข้อมูล
   deleteproduct(id: number) {
-    return this.http.delete('http://localhost/crudimg/delete.php?id='+id);
+    return this.http.delete('http://localhost/crudapi/delete.php?id='+id);
   }
 
   //เชื่อมต่อ api แก้ไขข้อมูล
   updateproduct(formData: FormData) {
     const headers = new HttpHeaders();
     // Remove Content-Type header to let browser set it automatically with boundary
-    return this.http.post('http://localhost/crudimg/update.php', formData, {
+    return this.http.post('http://localhost/crudapi/update.php', formData, {
       headers: headers
     });
   }
